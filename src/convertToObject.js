304b6b46801c5930241dd264cb2804d64ee4ec6c
Daniel <dania.szumski@gmail.com>
2025-08-26 13:58:03 +0200

    Done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,19 @@
 'use strict';
-
 /**
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styleObject = {};
+  const styleRules = sourceString.split(';').map(rule => rule.trim()).filter(Boolean);
+
+  for (const rule of styleRules) {
+    const [property, value] = rule.split(':').map(item => item.trim());
+    styleObject[property] = value;
+  }
+
+  return styleObject;
 }
 
 module.exports = convertToObject;
